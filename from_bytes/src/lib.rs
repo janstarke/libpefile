1fee1e55bd3dc79a5b464480635107ba7f1ea0b5
Jan Starke <jan.starke@outofbed.org>
2021-07-02 10:14:34 +0200

    added documentation for from_bytes

--- from_bytes/src/lib.rs
+++ from_bytes/src/lib.rs
@@ -2,11 +2,22 @@ use std::mem::size_of;
 use duplicate::duplicate;
 
 pub trait StructFromBytes<T: PackedSize = Self>: PackedSize {
+    /// Creates an instance of `T` by parsing a slice of bytes.
+    /// 
+    /// This method assumes that the byte slice contains unaligned data.
     fn from_bytes(slice: &[u8], offset: usize) -> std::io::Result<Box<Self>>;
 }
 
 
 pub trait PackedSize {
+    /// Returns the packed size of the struct. 
+    /// 
+    /// What is th packed size? Normally, struct members are aligned in a way that optimizes
+    /// access speed. For example, on a 64bit platform, all members are aligned to start
+    /// on a 64bit boundary [https://doc.rust-lang.org/reference/type-layout.html](https://doc.rust-lang.org/reference/type-layout.html).
+    /// 
+    /// *Packed* means what would be the size if no alignment would be used. In fact,
+    /// packed size equals the sum of sizes of all members.
     fn packed_size() -> usize;
 }
 
